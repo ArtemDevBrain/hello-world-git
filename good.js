594520f50bf68a942608a11afd7bfb9542fc2096
Artem <artnovorai@gmail.com>
2016-11-11 01:53:51 +0200

    added 3 file html css js

---
+++ good.js
@@ -0,0 +1,8 @@
+var elem = document.getElementById("t");
+        t.onclick = function () {
+        if(this.style.backgroundColor)
+ this.style.backgroundColor = "";
+ else
+this.style.backgroundColor = "red";
+
+      };
